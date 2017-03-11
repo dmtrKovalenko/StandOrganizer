5cf37f565476404450c618f78ee9260dc3d66757
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2017-03-11 23:00:26 +0200

    Setup EF code first, implemented initial test database for web api

---
+++ StandOrganizer.Api/StandOrganizer.DAL/EF/MyContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using StandOrganizer.DAL.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace StandOrganizer.DAL.EF
+{
+    public class MyContextFactory : IDbContextFactory<ApplicationDBContext>
+    {
+        public ApplicationDBContext Create()
+        {
+            return new ApplicationDBContext("DefaultConnection");
+        }
+    }
+}
--- StandOrganizer.Api/StandOrginizer.BL/Services/ServiceManager.cs
+++ StandOrganizer.Api/StandOrginizer.BL/Services/ServiceManager.cs
@@ -5,24 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StandOrginizer.BL.ViewModels;
+using StandOrganizer.DAL.UnitOfWork;
+using StandOrganizer.DAL.Repository;
+using StandOrganizer.DAL.Models;
 
 namespace StandOrginizer.BL.Services
 {
     public class ServiceManager : IServiceManager
     {
+        private readonly IRepository<Stand> serviceRepository;
+    
+        public ServiceManager(IUnitOfWork db)
+        {
+            this.serviceRepository = db.Repository<Stand>();
+        }
+
         public IEnumerable<ServiceViewModel> GetAllServices()
         {
-            return new List<ServiceViewModel>
-            {
-                new ServiceViewModel ()
-                {
-                    Id = 1
-                },
-                new ServiceViewModel ()
-                {
-                    Id = 2
-                }
-            };
+            var services = serviceRepository.Queryable.Select(x => new ServiceViewModel { Id = x.Id});
+
+            return services;
         }
     }
 }
